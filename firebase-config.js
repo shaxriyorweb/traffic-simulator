3088652894429c4b7f9da11f74b4b9d0a9c16f92
shaxriyorweb <choliyevshaxriyor6@gmail.com>
2025-05-15 14:31:21 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase konfiguratsiyasi - o'z Firebase loyihangizga moslab o'zgartiring
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase-ni boshlash
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
